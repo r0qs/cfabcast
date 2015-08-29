f4d4058c9e8465386a2427a95d57aee6141235c4
Rodrigo Q. Saramago <deepmarolaest@gmail.com>
2015-08-29 10:32:22 -0300

    Ignore console malformed input exception

--- src/main/scala/cfabcast/ConsoleClient.scala
+++ src/main/scala/cfabcast/ConsoleClient.scala
@@ -2,17 +2,24 @@ package cfabcast
 
 import akka.actor.Actor
 
+import java.nio.charset.CodingErrorAction
+
+import scala.io.Codec
+
 import cfabcast._
 import cfabcast.messages._
 
 class ConsoleClient extends Actor {
-  
+
   def receive = {
     case StartConsole => handleInput
   }
 
   def handleInput = {
-    for(cmd <- (io.Source.stdin.getLines.takeWhile(!_.equals("exit")))) {
+    implicit val codec = Codec.UTF8
+    codec.onMalformedInput(CodingErrorAction.IGNORE)
+    codec.onUnmappableCharacter(CodingErrorAction.IGNORE)
+    for(cmd <- (io.Source.fromInputStream(System.in).getLines.takeWhile(!_.equals("exit")))) {
       context.parent ! Command(cmd)
     }
   }
