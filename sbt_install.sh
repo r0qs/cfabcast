44ab5d4a67a78ba0409a951ba32a5d7c556eb617
Rodrigo Q. Saramago <deepmarolaest@gmail.com>
2015-10-15 16:49:51 -0300

    Update install script

--- sbt_install.sh
+++ sbt_install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# This script download and extract the sbt 0.13.0 file, removing the .tgz and put uncompressed file in ./sbt directory.
+# This script download and extract the sbt 0.13.9 file, removing the .tgz and put uncompressed file in ./sbt directory.
+
+# Requires Java runtime!
 
 # Over proxy, uncomment this line with your proxy settings
-#export http_proxy="http://proxy.ufu.br:3128"
+export SBT_OPTS="$SBT_OPTS -Dhttp.proxyHost=proxy.ufu.br -Dhttp.proxyPort=3128"
 
 wget -c -O - https://dl.bintray.com/sbt/native-packages/sbt/0.13.9/sbt-0.13.9.tgz | tar -xvzf -
+
+sleep 5
+
+sbt/bin/sbt compile
