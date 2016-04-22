c6152a2759b436faf0cfc6d68f48587926dc5b3d
Rodrigo Q. Saramago <deepmarolaest@gmail.com>
2016-04-22 16:00:46 -0300

    Little fix

--- src/main/scala/cfabcast/agents/Proposer.scala
+++ src/main/scala/cfabcast/agents/Proposer.scala
@@ -75,7 +75,8 @@ trait Proposer extends ActorLogging {
   def phase2Start(msg: Msg1B, state: Future[ProposerMeta], config: ClusterConfiguration)(implicit ec: ExecutionContext): Future[ProposerMeta] = async {
     val oldState = await(state)
     val quorum = quorumPerInstance.getOrElse(msg.instance, scala.collection.mutable.Map())
-    if (quorum.size >= config.quorumSize && isCoordinatorOf(msg.rnd) && crnd == msg.rnd && oldState.cval == None) {
+    // FIXME: Enter only one time here! Using == instead of >= defined in the specification
+    if (quorum.size == config.quorumSize && isCoordinatorOf(msg.rnd) && crnd == msg.rnd && oldState.cval == None) {
       val msgs = quorum.values.asInstanceOf[Iterable[Msg1B]]
       val k = msgs.reduceLeft((a, b) => if(a.vrnd > b.vrnd) a else b).vrnd
       val S = msgs.filter(a => (a.vrnd == k) && (a.vval != None)).map(a => a.vval).toList.flatMap( (e: Option[VMap[AgentId, Values]]) => e)
